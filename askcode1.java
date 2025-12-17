19ee08b8f1f5dbde0c3a5c9383ce0f5b556c29c0
ABDULRAFI2007 <abdulrafi.n2007@gmail.com>
2025-12-17 21:42:43 +0530

    Using forloop program  will be commited

---
+++ askcode1.java
@@ -0,0 +1,18 @@
+public class askcode1 {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=4;i+=1){
+            for(int r=1;r<=i;r+=1){
+                System.out.print(r);
+            }
+            System.out.println();
+        }
+        for(int j =4;j>=1;j-=1){
+            for (int q=1;q<=j;q+=1){
+                System.out.print(q);
+            }
+            System.out.println();
+        }
+    }
+    
+}
--- enhancedforloop.java
+++ enhancedforloop.java
@@ -1,4 +1,4 @@
-public class School {
+public class enhancedforloop{
     public static void main(String[] args) {
 
         int num[] = {12, 45, 67, 89};
@@ -11,13 +11,14 @@ public class School {
         System.out.println("-----");
         String[] dum = {"one", "two"};
 
-        for (String ars : dum) {
+        for (String ars:dum) {
             System.out.println(ars);
-
+        }
         // Enhanced for-each loop
-        for (int var : num) {
+        for (int var:num) {
             System.out.println(var);
             System.out.println();
-        }
+        
     }
+}
 }
